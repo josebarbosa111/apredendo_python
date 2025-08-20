01b352a098f35468aec1c12dc07932db8c2c3684
Jose Carlos <jose.carlos.s.barbosa@gmail.com>
2025-08-20 13:01:04 -0300

    Utilização de variaveis e exibindo dados

---
+++ aula01.py
@@ -0,0 +1,16 @@
+# Definindo variáveis
+nome = "Jose Carlos"
+idade = 35
+altura = 1.70
+situacaoTrabalho = True
+
+
+#Exibindo os dados com a função print
+
+# print('A soma é:', 2+3 )
+# print('A Multiplicação{}'.format(5*5))
+# print(f'A divisão é {10/2}')
+
+print(f'Seu nome é {nome}, sua idade é {idade} anos, sua altura {altura}, sua situação de trabalho é')
+
+print('Seu nome é', nome)
